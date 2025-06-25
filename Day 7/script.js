6593f9c62328d39d5ecec1a28b64900beeb39f0f
Hasan Yusuf Khan <116283180+YusufKhan2016@users.noreply.github.com>
2025-06-25 11:26:28 +0600

    relative name added

--- Day 7/script.js
+++ Day 7/script.js
@@ -1,3 +1,26 @@
-Vue.config.keyCodes = {
-    // camelCase won`t work
-}
+window.addEventListener("wheel", (dets)=> {
+    if(dets.deltaY>0){  
+        gsap.to(".marque", {
+        transform: "translateX(-100%)",
+        duration:3,
+        repeat: -1,
+        ease: "none",
+        })
+
+        gsap.to("img", {
+            rotate: 180,
+        })  
+
+    }else{
+        gsap.to(".marque", {
+            transform: "translateX(0%)",
+            duration:3,
+            repeat: -1,
+            ease: "none",
+        })
+
+        gsap.to("img", {
+            rotate: 0,
+        })
+    }
+})
